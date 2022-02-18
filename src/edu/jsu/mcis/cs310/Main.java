bd9a26890ed9bb8ff2b59be8dec9274840de9238
Charlie Owen <cowen6@stu.jus.edu>
2022-02-18 13:02:34 -0600

    Testing using Main

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -8,9 +8,20 @@ public class Main {
         
         Database db = new Database("cs310_p2_user", "P2!user", "localhost");
         
-        if (db.isConnected())
+        if (db.isConnected()) {
             
             System.err.println("Connected Successfully!");
+            
+            int result = db.register(1, 1, 22574);
+            String section = db.getSectionsAsJSON(1, "CS", "310");
+            String list = db.getScheduleAsJSON(1, 1);
+            
+            System.err.println(result);
+            System.err.println(section);
+            System.err.println(list);
+            
+        }
+        
         
     }
     
